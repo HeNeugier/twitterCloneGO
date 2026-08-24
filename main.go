b2eba7f5b06774278f9513dc2aa4301a2f195d6f
HeNeugier <github.guidable631@passmail.net>
2026-08-24 21:09:23 +0100

    handlers for metrics

--- main.go
+++ main.go
@@ -1,28 +1,57 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func main() {
+	//-- Define Constants --
+	const filepathRoot = "."
+	const port = "8080"
+
+	//-- Create our traffic director (mux: multiplexer) and config struct --
+	myMux := http.NewServeMux()
+	apiCfg := &apiConfig{}
+
+	//-- Muxxing Handlers start here --
+	//---------------------------------
+	myMux.HandleFunc("/healthz", readinessHandler)
+	//- middleware muxxing
+	myMux.HandleFunc("/metrics", apiCfg.metricsHandler)
+	myMux.HandleFunc("/reset", apiCfg.metricsResetHandler)
+	
+	//- Helpers
+	fileServer := http.FileServer(http.Dir(filepathRoot))
+	appHandler := http.StripPrefix("/app", fileServer)
+
+	//- wraps with middleware for metrics
+	myMux.Handle(
+		"/app/", 
+		apiCfg.metricsIncMiddleware(appHandler),
+	)
+
+	//-----Mux-ends-here---------------
+
+	//-- Define our server using the struct --
+	server := &http.Server{
+		Addr:				":" + port,
+		Handler:		myMux,
+	}
+
+	//-- Begin listening for HTTP --
+	server.ListenAndServe()
+
+}
 
 func readinessHandler(w http.ResponseWriter, _ *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("OK"))
+	fmt.Fprint(w, "OK")
 }
 
-func main() {
-	myMux := http.NewServeMux()
-	myMux.HandleFunc("/healthz", readinessHandler)
-	myMux.Handle(
-		"/app/", 
-		http.StripPrefix("/app", http.FileServer(http.Dir("."))),
-	)
-
-	s := &http.Server{
-		Addr:				":8080",
-		Handler:		myMux,
-	}
-
-	s.ListenAndServe()
-
-}
---
+++ metrics.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, _ *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Hits: %d", cfg.fileserverHits.Load())
+}
+
+func (cfg *apiConfig) metricsIncMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) metricsResetHandler(w http.ResponseWriter, _ *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Hits reset to 0"))
+}
